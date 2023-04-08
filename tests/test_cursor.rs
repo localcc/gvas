5bb271f2802226b71d6f6022bb36d7edff3dec18
Scott Anderson <scotta@gmail.com>
2023-04-07 23:40:34 -0400

    Add more cursor tests

--- tests/test_cursor.rs
+++ tests/test_cursor.rs
@@ -19,5 +19,72 @@ fn test_write_string() -> Result<(), Error> {
         cursor.get_ref(),
         &[0xfeu8, 0xffu8, 0xffu8, 0xffu8, 0xa7u8, 0u8, 0u8, 0u8],
     );
+
+    Ok(())
+}
+
+#[test]
+fn test_write_string_opt() -> Result<(), Error> {
+    // ASCII
+    let mut cursor = Cursor::new(Vec::new());
+    cursor.write_string_opt(Some("test"))?;
+    assert_eq!(
+        cursor.get_ref(),
+        &[5u8, 0u8, 0u8, 0u8, b't', b'e', b's', b't', 0u8],
+    );
+
+    // Non-ASCII
+    let mut cursor = Cursor::new(Vec::new());
+    cursor.write_string_opt(Some("\u{A7}"))?;
+    assert_eq!(
+        cursor.get_ref(),
+        &[0xfeu8, 0xffu8, 0xffu8, 0xffu8, 0xa7u8, 0u8, 0u8, 0u8],
+    );
+
+    // Null
+    let mut cursor = Cursor::new(Vec::new());
+    cursor.write_string_opt(None)?;
+    assert_eq!(cursor.get_ref(), &[0u8; 4],);
+
+    Ok(())
+}
+
+#[test]
+fn test_read_string() -> Result<(), Error> {
+    // ASCII
+    let mut cursor = Cursor::new(vec![5u8, 0u8, 0u8, 0u8, b't', b'e', b's', b't', 0u8]);
+    let string = cursor.read_string()?;
+    assert_eq!(string, "test");
+
+    // Non-ASCII
+    let mut cursor = Cursor::new(vec![0xfeu8, 0xffu8, 0xffu8, 0xffu8, 0xa7u8, 0u8, 0u8, 0u8]);
+    let string = cursor.read_string()?;
+    assert_eq!(string, "\u{A7}");
+
+    // Null
+    let mut cursor = Cursor::new(vec![0u8; 4]);
+    let err = cursor.read_string().expect_err("Expected err");
+    assert_eq!(err.to_string(), "Invalid string size, got 0");
+
+    Ok(())
+}
+
+#[test]
+fn test_read_string_opt() -> Result<(), Error> {
+    // ASCII
+    let mut cursor = Cursor::new(vec![5u8, 0u8, 0u8, 0u8, b't', b'e', b's', b't', 0u8]);
+    let maybe_string = cursor.read_string_opt()?;
+    assert_eq!(maybe_string, Some("test".to_string()));
+
+    // Non-ASCII
+    let mut cursor = Cursor::new(vec![0xfeu8, 0xffu8, 0xffu8, 0xffu8, 0xa7u8, 0u8, 0u8, 0u8]);
+    let maybe_string = cursor.read_string_opt()?;
+    assert_eq!(maybe_string, Some("\u{A7}".to_string()));
+
+    // Null
+    let mut cursor = Cursor::new(vec![0u8; 4]);
+    let maybe_string = cursor.read_string_opt()?;
+    assert_eq!(maybe_string, None);
+
     Ok(())
 }
