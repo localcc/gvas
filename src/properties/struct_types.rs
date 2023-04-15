b37289af582ff6387187c6b3bf464ab938bc98af
Scott Anderson <scotta@gmail.com>
2023-04-15 15:28:23 -0400

    Template struct_types

--- src/properties/struct_types.rs
+++ src/properties/struct_types.rs
@@ -2,137 +2,99 @@ use std::{fmt::Display, hash::Hash};
 
 use ordered_float::OrderedFloat;
 
-/// A struct that stores a vector.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub struct Vector {
-    /// X coordinate.
-    pub x: OrderedFloat<f32>,
-    /// Y coordinate.
-    pub y: OrderedFloat<f32>,
-    /// Z coordinate.
-    pub z: OrderedFloat<f32>,
+macro_rules! wrap_type {
+    (f32) => {
+        OrderedFloat<f32>
+    };
+    ($type:ident) => {
+        $type
+    };
 }
 
-impl Vector {
-    /// Creates a new `Vector` instance.
-    pub fn new(x: f32, y: f32, z: f32) -> Self {
-        let x = OrderedFloat::from(x);
-        let y = OrderedFloat::from(y);
-        let z = OrderedFloat::from(z);
-        Vector { x, y, z }
-    }
+macro_rules! wrap_value {
+    (f32, $name:ident) => {
+        OrderedFloat::from($name)
+    };
+    ($type:ident, $name:ident) => {
+        $type::from($name)
+    };
 }
 
-impl Display for Vector {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "x: {} y: {} z: {}", self.x, self.y, self.z)
-    }
-}
+macro_rules! make_struct {
+    (
+        $name:ident,
+        $topdoc:expr,
+        $(
+            ($field:ident, $type:ident, $doc:expr),
+        )+
+    ) => {
+        #[doc = $topdoc]
+        #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+        #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+        pub struct $name {
+            $(
+                #[doc = $doc]
+                pub $field: wrap_type!($type),
+            )+
+        }
 
-/// A struct that stores a rotator.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub struct Rotator {
-    /// Euclidean pitch.
-    pub pitch: OrderedFloat<f32>,
-    /// Euclidean yaw.
-    pub yaw: OrderedFloat<f32>,
-    /// Euclidean roll.
-    pub roll: OrderedFloat<f32>,
-}
+        impl $name {
+            #[doc = concat!("Creates a new `", stringify!($name), "` instance.")]
+            pub fn new( $($field: $type,)+ ) -> Self {
+                $(
+                    let $field = wrap_value!($type, $field);
+                )+
+                $name {
+                    $($field, )+
+                }
+            }
+        }
 
-impl Rotator {
-    /// Creates a new `Rotator` instance.
-    pub fn new(pitch: f32, yaw: f32, roll: f32) -> Self {
-        let pitch = OrderedFloat::from(pitch);
-        let yaw = OrderedFloat::from(yaw);
-        let roll = OrderedFloat::from(roll);
-        Rotator { pitch, yaw, roll }
-    }
+        impl Display for $name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, concat!(
+                    $(
+                        stringify!($field), ": {} ",
+                    )+
+                ), $(self.$field, )+)
+            }
+        }
+    };
 }
+make_struct!(
+    Vector,
+    "A struct that stores a vector.",
+    (x, f32, "X coordinate."),
+    (y, f32, "Y coordinate."),
+    (z, f32, "Z coordinate."),
+);
 
-impl Display for Rotator {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "pitch: {} yaw: {} roll: {}",
-            self.pitch, self.yaw, self.roll
-        )
-    }
-}
+make_struct!(
+    Rotator,
+    "A struct that stores a rotator.",
+    (pitch, f32, "Euclidean pitch."),
+    (yaw, f32, "Euclidean yaw."),
+    (roll, f32, "Euclidean roll."),
+);
 
-/// A struct that stores a quaternion.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub struct Quat {
-    /// X component.
-    pub x: OrderedFloat<f32>,
-    /// Y component.
-    pub y: OrderedFloat<f32>,
-    /// Z component.
-    pub z: OrderedFloat<f32>,
-    /// Real component.
-    pub w: OrderedFloat<f32>,
-}
+make_struct!(
+    Quat,
+    "A struct that stores a quaternion.",
+    (x, f32, "X component."),
+    (y, f32, "Y component."),
+    (z, f32, "Z component."),
+    (w, f32, "Real component."),
+);
 
-impl Quat {
-    /// Creates a new `Quat` instance.
-    pub fn new(x: f32, y: f32, z: f32, w: f32) -> Self {
-        let x = OrderedFloat::from(x);
-        let y = OrderedFloat::from(y);
-        let z = OrderedFloat::from(z);
-        let w = OrderedFloat::from(w);
-        Quat { x, y, z, w }
-    }
-}
+make_struct!(
+    DateTime,
+    "A struct that stores a date and time.",
+    (ticks, u64, "Ticks."),
+);
 
-impl Display for Quat {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "x: {} y: {} z: {} w: {}", self.x, self.y, self.z, self.w)
-    }
-}
-
-/// A struct that stores a date and time.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub struct DateTime {
-    /// Ticks.
-    pub ticks: u64,
-}
-
-impl DateTime {
-    /// Creates a new `DateTime` instance.
-    pub fn new(ticks: u64) -> Self {
-        DateTime { ticks }
-    }
-}
-
-impl Display for DateTime {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "ticks: {}", self.ticks)
-    }
-}
-
-/// A struct that stores a 2D integer point.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub struct IntPoint {
-    /// X value.
-    pub x: i32,
-    /// Y value.
-    pub y: i32,
-}
-
-impl IntPoint {
-    /// Creates a new `IntPoint` instance.
-    pub fn new(x: i32, y: i32) -> Self {
-        IntPoint { x, y }
-    }
-}
-
-impl Display for IntPoint {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "x: {} y: {}", self.x, self.y)
-    }
-}
+make_struct!(
+    IntPoint,
+    "A struct that stores a 2D integer point.",
+    (x, i32, "X value."),
+    (y, i32, "Y value."),
+);
