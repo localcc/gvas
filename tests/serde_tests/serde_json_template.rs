2e8d29696490ec2d42cdc2f3588ea5b099d2437e
Scott Anderson <scottanderson@users.noreply.github.com>
2024-03-04 22:02:10 -0500

    bools, enums, floats, ints, names, strings (#81)

--- tests/serde_tests/serde_json_template.rs
+++ tests/serde_tests/serde_json_template.rs
@@ -222,16 +222,9 @@ fn array_int32() {
         ),
         r#"{
   "type": "ArrayProperty",
-  "property_type": "IntProperty",
-  "properties": [
-    {
-      "type": "IntProperty",
-      "value": 0
-    },
-    {
-      "type": "IntProperty",
-      "value": 1
-    }
+  "ints": [
+    0,
+    1
   ]
 }"#,
     )
@@ -399,16 +392,9 @@ fn array_bool() {
         ),
         r#"{
   "type": "ArrayProperty",
-  "property_type": "BoolProperty",
-  "properties": [
-    {
-      "type": "BoolProperty",
-      "value": false
-    },
-    {
-      "type": "BoolProperty",
-      "value": true
-    }
+  "bools": [
+    false,
+    true
   ]
 }"#,
     )
@@ -461,16 +447,9 @@ fn array_float() {
         ),
         r#"{
   "type": "ArrayProperty",
-  "property_type": "FloatProperty",
-  "properties": [
-    {
-      "type": "FloatProperty",
-      "value": 1.0
-    },
-    {
-      "type": "FloatProperty",
-      "value": 2.0
-    }
+  "floats": [
+    1.0,
+    2.0
   ]
 }"#,
     )
@@ -478,6 +457,30 @@ fn array_float() {
 
 #[test]
 fn array_enum() {
+    serde_json(
+        &Property::ArrayProperty(
+            ArrayProperty::new(
+                String::from("EnumProperty"),
+                None,
+                vec![
+                    Property::EnumProperty(EnumProperty::new(None, "a".to_string())),
+                    Property::EnumProperty(EnumProperty::new(None, "b".to_string())),
+                ],
+            )
+            .expect("ArrayProperty::new"),
+        ),
+        r#"{
+  "type": "ArrayProperty",
+  "enums": [
+    "a",
+    "b"
+  ]
+}"#,
+    )
+}
+
+#[test]
+fn array_enum_ns() {
     serde_json(
         &Property::ArrayProperty(
             ArrayProperty::new(
@@ -527,6 +530,33 @@ fn array_name() {
         ),
         r#"{
   "type": "ArrayProperty",
+  "names": [
+    null,
+    "b"
+  ]
+}"#,
+    )
+}
+
+#[test]
+fn array_name_array_index() {
+    serde_json(
+        &Property::ArrayProperty(
+            ArrayProperty::new(
+                String::from("NameProperty"),
+                None,
+                vec![
+                    Property::NameProperty(NameProperty::from(None)),
+                    Property::NameProperty(NameProperty {
+                        array_index: 1,
+                        value: Some("b".to_string()),
+                    }),
+                ],
+            )
+            .expect("ArrayProperty::new"),
+        ),
+        r#"{
+  "type": "ArrayProperty",
   "property_type": "NameProperty",
   "properties": [
     {
@@ -534,6 +564,7 @@ fn array_name() {
     },
     {
       "type": "NameProperty",
+      "array_index": 1,
       "value": "b"
     }
   ]
@@ -588,15 +619,9 @@ fn array_str() {
         ),
         r#"{
   "type": "ArrayProperty",
-  "property_type": "StrProperty",
-  "properties": [
-    {
-      "type": "StrProperty"
-    },
-    {
-      "type": "StrProperty",
-      "value": "b"
-    }
+  "strings": [
+    null,
+    "b"
   ]
 }"#,
     )
